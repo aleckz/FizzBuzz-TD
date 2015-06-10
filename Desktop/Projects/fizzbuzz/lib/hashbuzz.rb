088713c6e68d451d573a13459146bb282042f0e0
Alex Nasr <aleksandernasr@gmail.com>
2015-06-10 10:06:43 +0100

    Update hashbuzz.rb

--- Desktop/Projects/fizzbuzz/lib/hashbuzz.rb
+++ Desktop/Projects/fizzbuzz/lib/hashbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
 
 	hash = {"fizzbuzz" => number % 3 == 0 && number % 5 == 0,
-				  "fizz" => number % 3 == 0 && number % 5 != 0,
-          "buzz" => number % 5 == 0 && number % 3 != 0
-          }
+	        "fizz" => number % 3 == 0 && number % 5 != 0,
+                "buzz" => number % 5 == 0 && number % 3 != 0
+                 }
 
 hash.key(true) || number
 
